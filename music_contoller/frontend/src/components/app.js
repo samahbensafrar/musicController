2c4a7a66da788bb49a2e64ddf78f2c72e27133b0
samah_bensafrar <samahbensafrar@gmail.com>
2025-06-04 09:32:07 -1200

    testing

---
+++ music_contoller/frontend/src/components/app.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <h1>Testing React Code</h1>;
+    }
+}
---
+++ music_contoller/music_contoller/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for music_contoller project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_contoller.settings')
+
+application = get_wsgi_application()
