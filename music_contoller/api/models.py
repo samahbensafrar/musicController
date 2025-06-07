2c761986635f5b4fd91ae9b12cc4e860b0da05b0
samah_bensafrar <samahbensafrar@gmail.com>
2025-06-06 12:45:32 -1200

    starting the components

--- music_contoller/api/models.py
+++ music_contoller/api/models.py
@@ -6,7 +6,7 @@ import string, random
 def generate_unique_code():
     length = 6
     while True :
-        code = "".join(random.choice(string.ascii_uppercase, k=length))
+        code = "".join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0 :
             break
     return code
@@ -14,8 +14,8 @@ def generate_unique_code():
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True) 
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True) 
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    vote_to_skip = models.IntegerField(null=False, default=1)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
--- music_contoller/frontend/src/components/CreateRoomPage.js
+++ music_contoller/frontend/src/components/CreateRoomPage.js
@@ -1,12 +1,70 @@
 import React, {Component} from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+
+
 
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
     constructor(props) {
         super(props);
     }
     render() {
-        return <p>this is the create room Page</p> 
+        return (
+                <Grid container spacing={1} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: "100vh" }}>
+                    <Grid item xs={12}>
+                        <Typography component="h4" variant="h4" align="center">
+                        Create A Room
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                        <FormHelperText>
+                            <div style={{ textAlign: "center" }}>
+                            Guest Control Of Playback State
+                            </div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue="true">
+                            <FormControlLabel
+                            value="true"
+                            control={<Radio color="primary" />}
+                            label="Play/Pause"
+                            labelPlacement="bottom"/>
+                            <FormControlLabel
+                            value="false"
+                            control={<Radio color="secondary" />}
+                            label="No Conrol/Pause"
+                            labelPlacement="bottom"/>
+                                
+                        </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                     <Grid container spacing={1} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: "100vh" }}>
+                        <FormControl>
+                            <TextField
+                            required="true"
+                            type="number"
+                            defaultValue={this.defaultVotes}
+                            inputProps={{
+                                min :1,
+                            }}/>
+                            <FormHelperText>
+                                <div align="center"> vote required to skip song</div>
+                            </FormHelperText>
+                        </FormControl>
+                     </Grid>
+                </Grid>
+
+               
+        );
 
     }
 }
